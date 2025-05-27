d31ded6d9eb84137bf25bcd981ae3048d4cda1f4
unknown <silvabryan203@gmail.com>
2025-05-27 08:07:39 -0300

    Atividade sobre vetores

--- atividade.js
+++ atividade.js
@@ -1,31 +1,31 @@
+let media = 0
 let num
+let soma = 0
 let numeros = []
-let outroNum
-let contador = 0
+let acimaMedia = []
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 8; i++) {
+
     do {
-        num = parseFloat(prompt(`Digite o ${i +1}º número`))
+        num = parseFloat(prompt("Digite um número"))
 
         if (isNaN(num) || num < 0)
             alert("Número Inválido")
 
     } while (isNaN(num) || num < 0)
+
+    soma += num
     numeros[i] = num
 }
 
-do {
+media = soma / 8
 
-    outroNum = parseFloat(prompt("Digite um número que você deseja saber quantas vezes aparece na sequência"))
-
-    if (isNaN(outroNum) || outroNum < 0) {
-        alert("Número inválido!");
-    }
-} while (isNaN(outroNum) || outroNum < 0);
-
-for (let j = 0; j < 10; j++) {
-    if (numeros[j] === outroNum) {
-        contador++;
+for (let j = 0; j < 8; j++) {
+    if (numeros[j] > media) {
+        acimaMedia[j] = numeros[j]
     }
 }
-alert(`O número digitado aparece ${contador} vezes`)
+
+alert(`A media dos números é ${media}`)
+
+alert(`Os números acima da média são ${acimaMedia}`)
